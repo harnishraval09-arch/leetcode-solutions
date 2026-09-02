8b454a523e5a2e661dd13db7457559504730e8bd
Harnish S Raval <harnishraval09@gmail.com>
2026-09-02 20:14:24 +0530

    Time: 2 ms (30.77%) | Memory: 97.4 MB (61.41%) - LeetSync

---
+++ 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+
+        int minP = prices[0];
+        int maxP = 0;
+
+        for (int i = 1; i < prices.size(); i++) {
+
+            if (prices[i] < minP) {
+                minP = prices[i];
+            }
+
+            int profit = prices[i] - minP;
+
+            if (profit > maxP) {
+                maxP = profit;
+            }
+        }
+
+        return maxP;
+    }
+};
